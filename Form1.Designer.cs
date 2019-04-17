2b1b098c1277403c75566680bfa8c79701c9b61a
mxb <357670442@qq.com>
2019-04-18 00:28:41 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("连接");
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.s7协议ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modbusTcpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oPCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.增加设备ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.s7协议ToolStripMenuItem,
+            this.modbusTcpToolStripMenuItem,
+            this.oPCToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(184, 88);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.ContextMenuStrip1_ItemClicked);
+            // 
+            // s7协议ToolStripMenuItem
+            // 
+            this.s7协议ToolStripMenuItem.Name = "s7协议ToolStripMenuItem";
+            this.s7协议ToolStripMenuItem.Size = new System.Drawing.Size(183, 28);
+            this.s7协议ToolStripMenuItem.Text = "S7协议";
+            this.s7协议ToolStripMenuItem.Click += new System.EventHandler(this.S7协议ToolStripMenuItem_Click);
+            // 
+            // modbusTcpToolStripMenuItem
+            // 
+            this.modbusTcpToolStripMenuItem.Name = "modbusTcpToolStripMenuItem";
+            this.modbusTcpToolStripMenuItem.Size = new System.Drawing.Size(183, 28);
+            this.modbusTcpToolStripMenuItem.Text = "ModbusTcp";
+            // 
+            // oPCToolStripMenuItem
+            // 
+            this.oPCToolStripMenuItem.Name = "oPCToolStripMenuItem";
+            this.oPCToolStripMenuItem.Size = new System.Drawing.Size(183, 28);
+            this.oPCToolStripMenuItem.Text = "OPC";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(-4, -1);
+            this.treeView1.Name = "treeView1";
+            treeNode1.ContextMenuStrip = this.contextMenuStrip1;
+            treeNode1.Name = "ConnectRoot";
+            treeNode1.Text = "连接";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.Size = new System.Drawing.Size(175, 479);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TreeView1_MouseClick);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem,
+            this.增加设备ToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(153, 60);
+            this.contextMenuStrip2.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.ContextMenuStrip2_ItemClicked);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(152, 28);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // 增加设备ToolStripMenuItem
+            // 
+            this.增加设备ToolStripMenuItem.Name = "增加设备ToolStripMenuItem";
+            this.增加设备ToolStripMenuItem.Size = new System.Drawing.Size(152, 28);
+            this.增加设备ToolStripMenuItem.Text = "增加设备";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.treeView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem s7协议ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modbusTcpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oPCToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 增加设备ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void S7协议ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+       
+
+        private void ContextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode();
+            treeNode1.Name = e.ClickedItem.Name;
+            treeNode1.Text = treeNode1.Name;
+            treeNode1.ContextMenuStrip = contextMenuStrip2;
+            this.treeView1.TopNode.Nodes.Add(treeNode1);
+            e.ClickedItem.Enabled = false;
+        }
+
+        private void ContextMenuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            TreeNode CurSelectedNode = treeView1.SelectedNode;
+            ToolStripItem Item1 = e.ClickedItem;
+            if (Item1.Equals(this.contextMenuStrip2.Items[0]))
+            {
+                treeView1.Nodes.Remove(CurSelectedNode);
+                contextMenuStrip1.Items[CurSelectedNode.Name].Enabled = true;
+
+            }
+                 
+   
+        }
+
+        private void TreeView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            TreeNode curNode=this.treeView1.GetNodeAt(e.Location);
+            if (curNode is null) return;
+            treeView1.SelectedNode = curNode;
+        }
+    }
+}
+
